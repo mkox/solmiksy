e0756c37436959774d07ab102d8a2134539d6603
Michael Kox <git1@michael-kox.de>
2015-12-07 11:20:35 +0100

    test for directive formEditCategory: first raw mock for vm.editCategory

--- src/Mkox/SolmikBundle/Resources/public/js/test/spec/cas-formEditCategory-directive2_spec.js
+++ src/Mkox/SolmikBundle/Resources/public/js/test/spec/cas-formEditCategory-directive2_spec.js
@@ -5,11 +5,9 @@
 //define(["angular", "angular-mocks", "cas/categoriesAndStrings", "cas/cas-formEditCategory-directive2c", "mkoxsolmik/partials/categoryEdit2.html"], function (angular, mocks, cas, editCategoryDirective) {
 //define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "mkoxsolmik/partials/categoryEdit2.html"], function (angular, mocks, editCategoryDirective) {
 
-define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "cas/cas-controller", "partials/categoryEdit2.html"], function (angular, mocks, editCategoryDirective, casController) {
-//define(["angular", "angular-mocks", "partials/categoryEdit2.html"], function (angular, mocks) {
-//define(["angular", "angular-mocks", "partials/categoryEdit2.html", "cas/cas-formEditCategory-directive2c"], function (angular, mocks, editCategoryDirective) {
-//    console.log('fEC-d2 editCategoryDirective: ', editCategoryDirective);
-//    console.log('fEC-d2 casController: ', casController);
+//define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "cas/cas-controller", "partials/categoryEdit2.html"], function (angular, mocks, editCategoryDirective, casController) {
+define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "partials/categoryEdit2.html"], function (angular, mocks, editCategoryDirective) {
+
 
 //define(["cas/categoriesAndStrings"], function () {
 
@@ -18,10 +16,11 @@ define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "cas
         var $scope;
         var $rootScope;
         var $compile;
+        var vm;
 //        var $location;
 
         beforeEach(module("categoriesAndStrings"));
-        
+
 //        beforeEach(module("cas/categoriesAndStrings"));
 //        beforeEach(module(cas));
 
@@ -31,7 +30,7 @@ define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "cas
 
         beforeEach(module("templates"));
 //        beforeEach(module("mkoxsolmik/partials/categoryEdit2.html"));
-        
+
 //        beforeEach(function () {
 ////            module('mkoxsolmik/partials/categoryEdit.html');
 //            module(function ($compileProvider) {
@@ -40,15 +39,45 @@ define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "cas
 //            });
 //        });
 
+        beforeEach(function () {
+//            module('PTTP.Directives.PowWow', function ($controllerProvider) {
+//                $controllerProvider.register('PowWowController', function ($scope) {
+//                    // Controller Mock
+//                    $scope.someVariable = SOME_VARIABLE;
+//                    $scope.someFunction = function () {
+//                    }; //{ //do stuff };
+//                });
+//            });
+            module(function ($controllerProvider) {
+                $controllerProvider.register('categoriesAndStringsCtrl', function ($scope) {
+                    // Controller Mock
+                    $scope.vm = {
+                        editCategory: function (event, category) {
+
+                        }
+                    };
+                });
+            });
+        });
+
         console.log('c-fEC-d2_test x100');
         beforeEach(inject(function (_$rootScope_, _$compile_, $controller) {
             console.log('c-fEC-d2_test x110');
+
             $rootScope = _$rootScope_;
             console.log('$rootScope 1(2):', $rootScope);
             $scope = $rootScope.$new();
+
+            $scope.category = {
+                "name": "Cat A",
+                "public": true
+            };
+
             var ctrl = $controller('categoriesAndStringsCtrl', {
                 $scope: $scope
+//                vm: vm
             });
+            console.log('c-fEC-d2_test ctrl: ', ctrl);
             $compile = _$compile_;
         }));
 //        beforeEach(function () {
@@ -78,12 +107,11 @@ define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "cas
         });
 
         it("(2) should edit a category name into 'new name'.", function () {
+
+//            spyOn(vm, "editCategory").and.callThrough();
+
 //            console.log('$rootScope 2(2):', $rootScope);
 //            var $scope = $rootScope.$new();
-            $scope.category = {
-                "name": "Cat A",
-                "public": true
-            };
 //            $scope.vm = casController.vm;
             console.log('$scope 1(2):', $scope);
 //            var element = $compile('<div class="step" ng-switch-when="edit" form-edit-category ng-attr-category="category" ng-attr-vm="vm">' +
@@ -97,7 +125,7 @@ define(["angular", "angular-mocks", "cas/cas-formEditCategory-directive2c", "cas
 
             var element = $compile('<div class="step"category="category" vm="vm" form-edit-category2c></div>')($scope);
 //            var element = $compile('<div class="step" form-edit-category></div>')($rootScope);
-            
+
 //            var element = $compile('<div class="step"><p>abcde</p></div>')($scope);
             console.log('2editC test element: ', element);
             console.log('2editC test $scope: ', $scope);
